ddfe9ade5568a8a31b9f5729b8077e74e98e0cf5
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-08-25 13:52:19 +0530

    x = int(input()) a = 0 b = 1 print(f"{a} {b}", end=' ') for i in range(1, x-1):     c = a+b     print(c, end=' ')     a=b     b=c

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,9 @@
-a = int(input())
-x = 0
-y = 1
-print(x,y,end=' ')
-for i in range(1, a-1):
-    z = x+y
-    print(z,end=' ')
-    x=y
-    y=z
+x = int(input())
+a = 0
+b = 1
+print(f"{a} {b}", end=' ')
+for i in range(1, x-1):
+    c = a+b
+    print(c, end=' ')
+    a=b
+    b=c
