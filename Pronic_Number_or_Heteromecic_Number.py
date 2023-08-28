2adbf829286da82e522112b018f1445737c40b4b
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-08-28 14:44:26 +0530

    x = int(input()) s=0 for i in range(1,x+1):     if i*(i+1)==x:         s=s+1 if s==1:     print("YES") else:     print("NO")

--- Pronic_Number_or_Heteromecic_Number.py
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -1,11 +1,9 @@
 x = int(input())
 s=0
-for i in range(1, x+1):
+for i in range(1,x+1):
     if i*(i+1)==x:
-        s=1
-        break;
+        s=s+1
 if s==1:
     print("YES")
 else:
     print("NO")
-        
