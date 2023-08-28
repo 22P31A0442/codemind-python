af0efda3c6f27926165172cf07e45af0ca070b06
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-08-28 14:35:20 +0530

    x = int(input()) y = int(input()) s=0 f=0 for i in range(1,x):     if x%i==0:         s=s+i for j in range(1,y):     if y%j==0:         f=f+j if x==f and y==s:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,14 @@
-a = int(input())
-b = int(input())
-s = 0
-f = 0
-for i in range(1, a):
-    if a%i==0:
-        s = s+i
-for j in range(1, b):
-    if b%j==0:
+x = int(input())
+y = int(input())
+s=0
+f=0
+for i in range(1,x):
+    if x%i==0:
+        s=s+i
+for j in range(1,y):
+    if y%j==0:
         f=f+j
-if s==b and f==a:
+if x==f and y==s:
     print("Amicable")
 else:
     print("Not Amicable")
