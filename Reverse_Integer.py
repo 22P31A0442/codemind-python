304de6825f6f24de791a282ccdc11ed159544c8f
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-09-05 19:32:29 +0530

    x = int(input()) s=0 if x>0:     while x!=0:         r=x%10         s=s*10+r         x=x//10     print(s) else:     n=abs(x)     while n!=0:         r=n%10         s=s*10+r         n=n//10     print(-s)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,15 +1,15 @@
-n = int(input())
+x = int(input())
 s=0
-if n>0:
-    while n!=0:
-        r=n%10
-        s=s*10+r
-        n=n//10
-    print(s)
-else:
-    x = abs(n)
+if x>0:
     while x!=0:
         r=x%10
         s=s*10+r
         x=x//10
+    print(s)
+else:
+    n=abs(x)
+    while n!=0:
+        r=n%10
+        s=s*10+r
+        n=n//10
     print(-s)
