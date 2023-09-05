f3a53c37a433e47a3c6a2af77e3bce9816c993d4
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-09-05 18:48:34 +0530

    n = int(input()) s=0 ss=0 sss=0 while n!=0:     r=n%10     s=r+s     n=n//10 if s<10:     print(s) else:     while s!=0:         rr=s%10         ss=rr+ss         s=s//10     if ss<10:         print(ss)     else:         while ss!=0:             rrr=ss%10             sss=sss+rrr             ss=ss//10         print(sss)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,23 +1,23 @@
 n = int(input())
 s=0
-f=0
-x=0
+ss=0
+sss=0
 while n!=0:
     r=n%10
-    s=s+r
+    s=r+s
     n=n//10
 if s<10:
     print(s)
 else:
     while s!=0:
-        rr = s%10
-        f=f+rr
+        rr=s%10
+        ss=rr+ss
         s=s//10
-    if f<10:
-        print(f)
+    if ss<10:
+        print(ss)
     else:
-        while f!=0:
-            rrr = f%10
-            x = x+rrr
-            f=f//10
-        print(x)
+        while ss!=0:
+            rrr=ss%10
+            sss=sss+rrr
+            ss=ss//10
+        print(sss)
