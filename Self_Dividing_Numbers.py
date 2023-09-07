7f1d053a38a25f337370052f8c6d5a82c3e8b98d
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-09-07 14:18:07 +0530

    a = int(input()) b = int(input()) s=0 f=0 x=0 for i in range(a, b+1):     q=i     if i<10:         print(i, end=' ')     else:         while q!=0:             r = q%10             s=s+1             q=q//10             if r==0:                 break             elif i%r==0:                 f=f+1         if s==f:             print(i, end=' ')         f=0         s=0         x=0

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,23 +1,24 @@
 a = int(input())
 b = int(input())
 s=0
-x=0
 f=0
+x=0
 for i in range(a, b+1):
     q=i
     if i<10:
-        print(i, end= ' ')
+        print(i, end=' ')
     else:
         while q!=0:
-            r=q%10
+            r = q%10
             s=s+1
+            q=q//10
             if r==0:
-                x=x+1
+                break
             elif i%r==0:
                 f=f+1
-            q=q//10
-        if f==s:
+        if s==f:
             print(i, end=' ')
-        s=0
         f=0
+        s=0
         x=0
+            
