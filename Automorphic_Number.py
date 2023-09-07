a849248e37bf130e265d15b85405db2a7be1a0c6
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-09-07 14:29:24 +0530

    n = int(input()) x = len(str(n)) sq=n**2 q=sq while q!=0:     r = q%(10**x)     if r==n:         print("Automorphic Number")         break     else:         print("Not an Automorphic Number")         break

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,15 +1,13 @@
 n = int(input())
-sq = n**2
-q=n
-s=0
+x = len(str(n))
+sq=n**2
+q=sq
 while q!=0:
-    r=q%10
-    s=s+1
-    q=q//10
-x = 10**s
-rr=sq%x
-if rr==n:
-    print("Automorphic Number")
-else:
-    print("Not an Automorphic Number")
+    r = q%(10**x)
+    if r==n:
+        print("Automorphic Number")
+        break
+    else:
+        print("Not an Automorphic Number")
+        break
     
