b5d4324d15d59b5755c51f09557892cd24e1fc7b
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-12 15:04:23 +0530

    n = int(input()) x = list(map(int, input().split())) s = int(input()) print(s in x)

--- Element_search_in_an_Array.py
+++ Element_search_in_an_Array.py
@@ -1,11 +1,4 @@
 n = int(input())
-a = list(map(int, input().split()))
-x = int(input())
-s=0
-for i in range(0,n):
-    if a[i]==x:
-        s=s+1
-if s>=1:
-    print("True")
-else:
-    print("False")
+x = list(map(int, input().split()))
+s = int(input())
+print(s in x)
