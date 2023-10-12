a5c9a9ec7d8f3603d0daf91fb92e2152a7c64ccc
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-12 15:06:15 +0530

    n = int(input()) x = list(map(int, input().split())) print((sum(x))//n in x)

--- Average_element_is_in_Array_or_Not.py
+++ Average_element_is_in_Array_or_Not.py
@@ -1,15 +1,3 @@
-def sls(n, a, s):
-    f=0
-    for i in range(0, n):
-        if a[i]==s:
-            f=f+1
-    if f>=1:
-        return True
-    else:
-        return False
-
-
 n = int(input())
-a = list(map(int, input().split()))
-s = sum(a)//n
-print(sls(n,a,s))
+x = list(map(int, input().split()))
+print((sum(x))//n in x)
