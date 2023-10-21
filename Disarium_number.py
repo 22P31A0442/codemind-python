0c8485e2c1b339af9346553091c0fe78f742051e
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-21 16:03:17 +0530

    n = int(input()) s=0 x = len(str(n)) q=n while q!=0:     r=q%10     s=s+r**x     x=x-1     q=q//10 if n==s:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,15 +1,13 @@
 n = int(input())
-x = len(str(n))
 s=0
-y=0
+x = len(str(n))
 q=n
 while q!=0:
-    r = q%10
-    s=s+1
-    m = r**(x+1-s)
-    y=y+m
+    r=q%10
+    s=s+r**x
+    x=x-1
     q=q//10
-if n==y:
+if n==s:
     print("True")
 else:
     print("False")
