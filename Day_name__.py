5bae3f854995459d89f07f897649e27fe51acd0e
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-22 10:35:04 +0530

    def sls(n):     if n==1:         return 'Monday'     elif n==2:         return 'Tuesday'     elif n==3:         return 'Wednesday'     elif n==4:         return 'Thursday'     elif n==5:         return 'Friday'     elif n==6:         return 'Saturday'     elif n==7:         return 'Sunday'     else:         return 'Invalid input' n = int(input()) print(sls(n))

--- Day_name__.py
+++ Day_name__.py
@@ -1,17 +1,19 @@
-a = int(input())
-if a ==1:
-    print("Monday")
-elif a ==2:
-    print("Tuesday")
-elif a ==3 :
-    print("Wednesday")
-elif a ==4 :
-    print("Thursday")
-elif a ==5 :
-    print("Friday")
-elif a ==6 :
-    print("Saturday")
-elif a ==7 :
-    print("Sunday")
-else:
-    print("Invalid input")
+def sls(n):
+    if n==1:
+        return 'Monday'
+    elif n==2:
+        return 'Tuesday'
+    elif n==3:
+        return 'Wednesday'
+    elif n==4:
+        return 'Thursday'
+    elif n==5:
+        return 'Friday'
+    elif n==6:
+        return 'Saturday'
+    elif n==7:
+        return 'Sunday'
+    else:
+        return 'Invalid input'
+n = int(input())
+print(sls(n))
