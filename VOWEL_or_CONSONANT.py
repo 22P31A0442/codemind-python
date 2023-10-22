224cba8c710b4329a383a2ddd6075baed728a109
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-22 10:29:07 +0530

    def sls(a):     if a in 'aeiouAEIOU':         return 'VOWEL'     else:         return 'CONSONANT' a = input() print(sls(a))

--- VOWEL_or_CONSONANT.py
+++ VOWEL_or_CONSONANT.py
@@ -1,7 +1,7 @@
+def sls(a):
+    if a in 'aeiouAEIOU':
+        return 'VOWEL'
+    else:
+        return 'CONSONANT'
 a = input()
-if a == 'a' or a=='e' or a=='i' or a=='o' or a=='u':
-    print("VOWEL")
-elif a =='A' or a =='E' or a =='I' or a=='O' or a=='U':
-    print("VOWEL")
-else:
-    print("CONSONANT")
+print(sls(a))
