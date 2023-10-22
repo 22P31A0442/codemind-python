5180d3c731b13404ee8566e869ba8c04f8bca3e5
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-22 10:44:03 +0530

    def sls(n):     if n>=90:         return 'Grade A'     elif n>=80 and n<90:         return 'Grade B'     elif n>=70 and n<80:         return 'Grade C'     elif n>=60 and n<70:         return 'Grade D'     elif n>=40 and n<60:         return 'Grade E'     else:         return 'Grade F' a, b, c, d, e = map(int,input().split()) n = (a+b+c+d+e)//5 print(sls(n))

--- Grades.py
+++ Grades.py
@@ -1,14 +1,16 @@
-a, b, c, d, e = map(int, input().split())
-x = (a+b+c+d+e)/5
-if x>=90:
-    print("Grade A")
-elif x >=80 and x <90:
-    print("Grade B")
-elif x >=70 and x <80:
-    print("Grade C")
-elif x >=60 and x <70:
-    print("Grade D")
-elif x >=40 and x <60:
-    print("Grade E")
-else :
-    print("Grade F")
+def sls(n):
+    if n>=90:
+        return 'Grade A'
+    elif n>=80 and n<90:
+        return 'Grade B'
+    elif n>=70 and n<80:
+        return 'Grade C'
+    elif n>=60 and n<70:
+        return 'Grade D'
+    elif n>=40 and n<60:
+        return 'Grade E'
+    else:
+        return 'Grade F'
+a, b, c, d, e = map(int,input().split())
+n = (a+b+c+d+e)//5
+print(sls(n))
