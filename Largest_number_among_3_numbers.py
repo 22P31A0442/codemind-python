a6ebbc71d7ed0a4ee226cc88a21870309104b64f
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-22 10:27:41 +0530

    def sls(a, b, c):     return max(a,b,c) a, b, c = map(int, input().split()) print(sls(a, b, c))

--- Largest_number_among_3_numbers.py
+++ Largest_number_among_3_numbers.py
@@ -1,7 +1,4 @@
+def sls(a, b, c):
+    return max(a,b,c)
 a, b, c = map(int, input().split())
-if a>b and a>c:
-    print(a)
-elif b>a and b>c:
-    print(b)
-else:
-    print(c)
+print(sls(a, b, c))
