ceb0b9beb9ffe32088af0cc3d18842aa8cb4910f
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-24 13:07:58 +0530

    def sls1(n):     print(f"{(n+(n*0.8)+(n*0.2)):.2f}") def sls2(n):     print(f"{(n+(n*0.90)+(n*0.25)):.2f}") def sls3(n):     print(f"{(n+(n*0.95)+(n*0.3)):.2f}") n = int(input()) if n<=10000:     sls1(n) elif n>10000 and n<=20000:     sls2(n) else:     sls3(n)

--- Gross_Salary.py
+++ Gross_Salary.py
@@ -1,10 +1,13 @@
-s = int(input())
-if s <= 10000:
-    bs = s + (s*80/100)+(s*20/100)
-    print(f"{bs:.2f}")
-elif s <= 20000 and s >= 10000:
-    bs = s+(s*90/100)+(s*25/100)
-    print(f"{bs:.2f}")
+def sls1(n):
+    print(f"{(n+(n*0.8)+(n*0.2)):.2f}")
+def sls2(n):
+    print(f"{(n+(n*0.90)+(n*0.25)):.2f}")
+def sls3(n):
+    print(f"{(n+(n*0.95)+(n*0.3)):.2f}")
+n = int(input())
+if n<=10000:
+    sls1(n)
+elif n>10000 and n<=20000:
+    sls2(n)
 else:
-    bs = s+(s*95/100)+(s*30/100)
-    print(f"{bs:.2f}")
+    sls3(n)
