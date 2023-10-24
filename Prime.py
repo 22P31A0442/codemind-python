41d8ebc1ef3b7527ee15a7d914241477be38fc21
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-24 20:14:14 +0530

    def sls(n):     s=0     for i in range(1, n+1):         if n%i==0:             s=s+1     if s==2:         return 'Prime'     else:         return 'Not Prime' n = int(input()) print(sls(n))

--- Prime.py
+++ Prime.py
@@ -1,9 +1,11 @@
-x = int(input())
-s=0
-for i in range(1, x+1):
-    if x%i==0:
-        s=s+1
-if s>2:
-    print("Not Prime")
-else:
-    print("Prime")
+def sls(n):
+    s=0
+    for i in range(1, n+1):
+        if n%i==0:
+            s=s+1
+    if s==2:
+        return 'Prime'
+    else:
+        return 'Not Prime'
+n = int(input())
+print(sls(n))
