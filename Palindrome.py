164471b27c16d801f83a2989b71703e1e1c9891a
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-24 20:19:48 +0530

    def sls(n):     q=n     s=0     while q!=0:         r=q%10         s=s*10+r         q=q//10     if s==n:         return 'Palindrome'     else:         return 'Not Palindrome' n = int(input()) print(sls(n))

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,13 @@
+def sls(n):
+    q=n
+    s=0
+    while q!=0:
+        r=q%10
+        s=s*10+r
+        q=q//10
+    if s==n:
+        return 'Palindrome'
+    else:
+        return 'Not Palindrome'
 n = int(input())
-s=0
-q=n
-while q!=0:
-    r=q%10
-    s=s*10+r
-    q=q//10
-if s==n:
-    print("True")
-else:
-    print("False")
+print(sls(n))
