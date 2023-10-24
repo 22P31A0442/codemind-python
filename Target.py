5d4c8d53a8b93aaea5afb5ea0e8742a548cda727
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-10-24 20:02:28 +0530

    def sls(a, b, c, d):     if a>=10 and b>=10 and c>=10 and d>=10:         return 'YES'     else:         return 'NO' a, b, c, d = map(int, input().split()) print(sls(a, b, c, d))

--- Target.py
+++ Target.py
@@ -1,5 +1,7 @@
+def sls(a, b, c, d):
+    if a>=10 and b>=10 and c>=10 and d>=10:
+        return 'YES'
+    else:
+        return 'NO'
 a, b, c, d = map(int, input().split())
-if a >=10 and b >= 10 and c >=10 and d >=10:
-    print("YES")
-else:
-    print("NO")
+print(sls(a, b, c, d))
